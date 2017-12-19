b2396fdc6b965c810bfd71a0c7ba0a426020f5ea
USUARIO <USUARIO@172.16.2.44>
2017-12-19 20:28:31 -0300

    Merge origin/master
    Conflicts:
    	src/controlador/Controlador.java

--- src/controlador/Controlador.java
+++ src/controlador/Controlador.java
@@ -2,8 +2,12 @@
 package controlador;
 
 import java.sql.Connection;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
+import javax.swing.JDialog;
+import javax.swing.JOptionPane;
 import modelo.Empleado;
 
 
@@ -30,11 +34,41 @@ public class Controlador {
        
        public void agregar(){
        }
-    public void eliminar (){
+       
+       
+       
+       public void eliminar (){
         
         
     }
-    
+       
+    public ArrayList<Persona> getLista()
+    {
+        ArrayList<Persona> persona = new ArrayList();
+        String sentencia = "SELECT id, rut, fecha_ingreso, direccion, fecha_cumpleaño, nombre FROM PERSONAL"; 
+        try {
+            Connection conexion = Conexion.getConnection();
+            Statement statement = conexion.createStatement();
+            ResultSet resultado = statement.executeQuery(sentencia);
+            
+            while (resultado.next()){
+   
+                int id =Integer.parseInt(resultado.getString("id"));
+                int rut =Integer.parseInt(resultado.getString("rut"));
+                String ingreso =resultado.getString("fecha_ingreso"));
+                String direccion =resultado.getString("direccion");
+                String cumple =resultado.getString("fecha_cumpleaño");
+                String nombre =resultado.getString("nombre");
+                
+                persona.add(new Persona(id, rut, fechaIngreso, fechaCumpleanos, direccion, nombre));
+            }
+        } catch (SQLException ex) {
+             JOptionPane.showMessageDialog(new JDialog(), "Error al listar.");
+        }
+        return peliculas;
+    }
+        
+    }
     
     
 }
