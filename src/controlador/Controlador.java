28007370e867ed430a69a533b00ac7ab47b62e7e
USUARIO <USUARIO@172.16.2.44>
2017-12-19 19:46:50 -0300

    Trabajo 15-12-2017

--- src/controlador/Controlador.java
+++ src/controlador/Controlador.java
@@ -1,14 +1,31 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package controlador;
 
-/**
- *
- * @author USUARIO
- */
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+import modelo.Empleado;
+
+
 public class Controlador {
+    
+       public void modificar(Empleado empleado, int rut) throws SQLException
+    {
+        String sentencia = "update empleado set id='"+empleado.getId()+"' where rut ="+rut;
+        String sentencia1 = "update empleado set rut='"+empleado.getRut()+"' where rut ="+rut;
+        String sentencia2 = "update empleado set fechaIngreso='"+empleado.getFechaIngreso()+"' where rut ="+rut;
+        String sentencia3 = "update empleado set fechaCumpleanos='"+empleado.getFechaCumpleanos()+"' where rut ="+rut;
+        String sentencia4 = "update empleado set direccion='"+empleado.getDireccion()+"' where rut ="+rut;
+        String sentencia5 = "update empleado set nombre='"+empleado.getNombre()+"' where rut ="+rut;
+        
+        Connection conexion = Conexion.getConnection();
+        Statement statement = conexion.createStatement();
+        statement.executeUpdate(sentencia);
+        statement.executeUpdate(sentencia1);
+        statement.executeUpdate(sentencia2);
+        statement.executeUpdate(sentencia3);
+        statement.executeUpdate(sentencia4);
+        statement.executeUpdate(sentencia5);
+    }
     
 }
--- src/vista/Principal.java
+++ src/vista/Principal.java
@@ -8,7 +8,7 @@ public class Principal extends javax.swing.JFrame {
     public Principal() {
         initComponents();
         this.setTitle("Proyecto RR.HH");
-        
+        this.setLocationRelativeTo(null);
         
     }
 
@@ -17,23 +17,64 @@ public class Principal extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        jMenuBar1 = new javax.swing.JMenuBar();
+        jMenu1 = new javax.swing.JMenu();
+        jMenu2 = new javax.swing.JMenu();
+        jMenuItem1 = new javax.swing.JMenuItem();
+
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        jMenu1.setText("File");
+        jMenuBar1.add(jMenu1);
+
+        jMenu2.setText("Edit");
+        jMenu2.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jMenu2ActionPerformed(evt);
+            }
+        });
+
+        jMenuItem1.setText("Modificar");
+        jMenuItem1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jMenuItem1ActionPerformed(evt);
+            }
+        });
+        jMenu2.add(jMenuItem1);
+
+        jMenuBar1.add(jMenu2);
+
+        setJMenuBar(jMenuBar1);
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 771, Short.MAX_VALUE)
+            .addGap(0, 746, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 507, Short.MAX_VALUE)
+            .addGap(0, 292, Short.MAX_VALUE)
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
+    private void jMenu2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenu2ActionPerformed
+        
+    }//GEN-LAST:event_jMenu2ActionPerformed
+
+    private void jMenuItem1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItem1ActionPerformed
+       Modificar modificar = new Modificar();
+        modificar.setVisible(true);
+        this.setVisible(false);
+    }//GEN-LAST:event_jMenuItem1ActionPerformed
+
    
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JMenu jMenu1;
+    private javax.swing.JMenu jMenu2;
+    private javax.swing.JMenuBar jMenuBar1;
+    private javax.swing.JMenuItem jMenuItem1;
     // End of variables declaration//GEN-END:variables
 }
