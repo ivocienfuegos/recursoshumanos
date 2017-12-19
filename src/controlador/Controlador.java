2da977e2a06784ddca60c63b7819ede4dccba926
Ivo <Ivo@172.16.2.137>
2017-12-19 19:56:15 -0300

    agregar

--- src/controlador/Controlador.java
+++ src/controlador/Controlador.java
@@ -27,5 +27,8 @@ public class Controlador {
         statement.executeUpdate(sentencia4);
         statement.executeUpdate(sentencia5);
     }
+       
+       public void agregar(){
+       }
     
 }
